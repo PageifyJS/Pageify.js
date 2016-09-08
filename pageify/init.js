6bda2dd1b05bdad24afc6179eccd8b0ead485d92
Hampton <herohamp@users.noreply.github.com>
2016-09-08 16:36:23 -0400

    Update init.js

--- pageify/init.js
+++ pageify/init.js
@@ -1,5 +1,6 @@
 function initiate(){
    retrievePage(default_load, "content");
    document.getElementById("init").innerHTML=load("./pageify/functions.js");
+   load("./pageify/routes.js");
    interpret()
 }
