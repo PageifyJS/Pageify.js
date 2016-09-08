3bf33c610f3a821a3799362f1b69dfc4bfa06996
Hampton <herohamp@users.noreply.github.com>
2016-09-08 19:22:03 -0400

    Update init.js

--- pageify/init.js
+++ pageify/init.js
@@ -1,19 +1,19 @@
-function loadScript(url) {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange=function() {
-    if (this.readyState == 4 && this.status == 200) {
-      var body = document.body.innerHTML;
-      document.body.innerHTML = body + "<script>" + this.responseText + "</script>";
-    }
-  };
-  xhttp.open("GET", url, true);
-  xhttp.send();
-}
-function initiate(){
-   loadScript('./pageify/functions.js');
-   loadScript('./pageify/config.js');
-   loadScript('./pageify/interpreter.js');
-   loadScript('./pageify/routes.js');
-  retrievePage("default_load", "content");
-   interpret()
+function initiate() {
+	function loadScript(url) {
+		var xhttp = new XMLHttpRequest();
+		xhttp.onreadystatechange = function() {
+			if (this.readyState == 4 && this.status == 200) {
+				var body = document.body.innerHTML;
+				document.body.innerHTML = body + "<script>" + this.responseText + "</script>";
+			}
+		};
+		xhttp.open("GET", url, true);
+		xhttp.send();
+	}
+	loadScript('./pageify/functions.js');
+	loadScript('./pageify/config.js');
+	loadScript('./pageify/interpreter.js');
+	loadScript('./pageify/routes.js');
+	retrievePage("default_load", "content");
+	interpret()
 }
