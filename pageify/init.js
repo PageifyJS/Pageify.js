18759b8561a26e57a9aad90d52e6a26ffa858222
Hampton <herohamp@users.noreply.github.com>
2016-09-08 19:23:42 -0400

    Update init.js

--- pageify/init.js
+++ pageify/init.js
@@ -1,14 +1,6 @@
 function initiate() {
 	function loadScript(url) {
-		var xhttp = new XMLHttpRequest();
-		xhttp.onreadystatechange = function() {
-			if (this.readyState == 4 && this.status == 200) {
-				var body = document.body.innerHTML;
-				document.body.innerHTML = body + "<script>" + this.responseText + "</script>";
-			}
-		};
-		xhttp.open("GET", url, true);
-		xhttp.send();
+		document.write("<script type='text/javascript' src='"+url+"'></script>"); 
 	}
 	loadScript('./pageify/functions.js');
 	loadScript('./pageify/config.js');
