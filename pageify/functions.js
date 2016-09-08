4e4609eed631c4fc81955a5afb60445940633b9e
HittmanA <hittmana@comcast.net>
2016-09-08 17:20:57 -0400

    Update functions.js

--- pageify/functions.js
+++ pageify/functions.js
@@ -3,7 +3,6 @@ function test() {
   Array.prototype.forEach.call(document.querySelectorAll("dynamicContent"), function(el) {
   var id = el.id
   id = id.replace('dynamic-update-', '');
-  alert(id)
 });
 }
 setInterval(function(){
