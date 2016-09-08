eff9bfe555e9752293ff3c9f6595aea8006ab508
HittmanA <hittmana@comcast.net>
2016-09-08 17:11:59 -0400

    Update loader.js

--- pageify/loader.js
+++ pageify/loader.js
@@ -6,6 +6,7 @@ client.onreadystatechange = function() {
   if (client.readyState == 4 && client.status == 200) {
   var data = client.responseText;
   document.getElementById(section).innerHTML=interpret(data);
+  test()
  }
 }
 client.send();
