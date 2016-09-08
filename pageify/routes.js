dfac55f8542ff06c61b3fef3c12c7ca2bceecacd
Hampton <herohamp@users.noreply.github.com>
2016-09-08 16:36:18 -0400

    Create routes.js

---
+++ pageify/routes.js
@@ -0,0 +1,30 @@
+var querys = window.location.search.slice(1).split('&').reduce(function _reduce (/*Object*/ a, /*String*/ b) {b = b.split('=');a[b[0]] = decodeURIComponent(b[1]);return a;}, {});
+console.log(querys);
+
+var page;
+
+if (querys[Object.keys(querys)[0]]== "undefined") {
+  page="/";
+} else {
+  page=Object.keys(querys)[0];
+}
+
+var routes = {};
+
+function loadPage(url) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange=function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.body.innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", url, true);
+  xhttp.send();
+}
+
+  for (i in routes) {
+    if (routes[i].route==page) {
+      loadPage(routes[i].pagelocation);
+      eval(routes[i].callback)
+    }
+  }
