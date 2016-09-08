4f3eafa11f89ae50673acc005a03e3b8af2444ba
HittmanA <hittmana@comcast.net>
2016-09-08 16:50:37 -0400

    Update init.js

--- pageify/init.js
+++ pageify/init.js
@@ -1,6 +1,5 @@
 function initiate(){
    retrievePage("default_load", "content");
    document.getElementById("init").innerHTML=load("./pageify/functions.js");
-   load("./pageify/routes.js");
    interpret()
 }
