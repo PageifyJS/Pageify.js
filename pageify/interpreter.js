4e7bbb0270bf7904dc0c88c2168512ad80922633
Hampton <herohamp@users.noreply.github.com>
2016-09-09 16:32:15 -0400

    Update interpreter.js

--- pageify/interpreter.js
+++ pageify/interpreter.js
@@ -5,7 +5,7 @@ if (data) {
 var myString = data;
 
  for(var i = 0; i < Object.keys(variables).length; i++) {
-    console.log(data)
+    typeof data;
      var extract = data.match(/{{(.*)}}/).pop();
      myString = myString.replace(new RegExp('{{' + extract + '}}', 'gi'), variables[extract]);
   }
