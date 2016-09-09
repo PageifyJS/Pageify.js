96b847957d6fa2f23985840965647c3b41b7df50
HittmanA <hittmana@comcast.net>
2016-09-09 16:28:49 -0400

    Update config.js

--- pageify/config.js
+++ pageify/config.js
@@ -12,7 +12,7 @@ var pages = {
  page1 : {"url":"./page1.html","callback":"function(){}","route":"/page1"},
  page2 : {"url":"./page2.html","callback":"function(){}","route":"/page2"},
  test : {"url":"https://crossorigin.me/https://scratch.mit.edu/discuss/topic/207160/?page=11#post-2196614","callback":"function(){}","route":"/test"},
- test2 : {"url":"http://ahitt-industries.com","callback":"function(){}","route":"/test"}
+ test2 : {"url":"http://ahitt-industries.com","callback":"function(){}","route":"/test2"}
 }
 
 retrievePage("default_load", "content");
