513f88b616a73c3388fa34aa297f475b8d67d789
HittmanA <hittmana@comcast.net>
2016-09-08 21:48:30 -0400

    Update init.js

--- pageify/init.js
+++ pageify/init.js
@@ -2,6 +2,5 @@ function initiate() {
 	function loadScript(url) {
 		document.write("<script type='text/javascript' src='"+url+"'></script>"); 
 	}
-	loadScript('./pageify/functions.js');
 	interpret()
 }
