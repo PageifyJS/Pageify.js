86c7fce7db0f0edced5c23fd374551ee25bc7c2a
HittmanA <hittmana@comcast.net>
2016-09-08 22:14:43 -0400

    Update init.js

--- pageify/init.js
+++ pageify/init.js
@@ -1,15 +1,15 @@
 function initiate() {
-	function loadScript(url) {
-        var xhttp = new XMLHttpRequest();
- -		xhttp.onreadystatechange = function() {
- -			if (this.readyState == 4 && this.status == 200) {
- -				var body = document.body.innerHTML;
- -				document.body.innerHTML = body + "<script>" + this.responseText + "</script>";
- -			}
- -		};
- -		xhttp.open("GET", url, true);
- -		xhttp.send();
- }
+ function loadScript(str) {
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                var body = document.body.innerHTML;
+                document.body.innerHTML = body + "<script>" + this.responseText + "</script>";
+            }
+        };
+        xmlhttp.open("GET", url, true);
+        xmlhttp.send();
+}
  loadScript("./loader.js");
 	interpret()
 }
