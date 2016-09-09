54ea3b7563ffcba87960aacfe08fca04f5c030de
HittmanA <hittmana@comcast.net>
2016-09-08 22:19:11 -0400

    Update loader.js

--- pageify/loader.js
+++ pageify/loader.js
@@ -11,15 +11,3 @@ client.onreadystatechange = function() {
 }
 client.send();
 }
-
-function loadScript(url) {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange=function() {
-    if (this.readyState == 4 && this.status == 200) {
-      var body = document.body.innerHTML;
-      document.body.innerHTML = body + "<script>" + this.responseText + "</script>";
-    }
-  };
-  xhttp.open("GET", url, true);
-  xhttp.send();
-}
