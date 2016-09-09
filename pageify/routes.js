9be5da77cb13ce589ef0cccf639072dadfabb0ee
HittmanA <hittmana@comcast.net>
2016-09-09 08:24:48 -0400

    Update routes.js

--- pageify/routes.js
+++ pageify/routes.js
@@ -15,7 +15,7 @@ function loadPage(url) {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange=function() {
     if (this.readyState == 4 && this.status == 200) {
-      retrievePage(url, "section");
+      retrievePage(url, "content");
       //document.body.innerHTML = this.responseText;
       interpret()
     }
