8f0cfc97196f082ba234e5cf404ad67f15fe20ad
HittmanA <hittmana@comcast.net>
2016-09-08 22:16:26 -0400

    Update init.js

--- pageify/init.js
+++ pageify/init.js
@@ -1,5 +1,5 @@
 function initiate() {
- function loadScript(str) {
+ function loadScript(url) {
         var xmlhttp = new XMLHttpRequest();
         xmlhttp.onreadystatechange = function() {
             if (this.readyState == 4 && this.status == 200) {
