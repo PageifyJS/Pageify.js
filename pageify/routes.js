705f80a64c9d5e4dcc98de7a1d41d6599a1d67db
HittmanA <hittmana@comcast.net>
2016-09-09 15:06:53 -0400

    Update routes.js

--- pageify/routes.js
+++ pageify/routes.js
@@ -11,22 +11,23 @@ if (querys[Object.keys(querys)[0]]!= "undefined") {
 
 var routes = {};
 
-function loadPage(url) {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange=function() {
-    if (this.readyState == 4 && this.status == 200) {
-      retrievePage(url, "content");
-      //document.body.innerHTML = this.responseText;
-      interpret()
-    }
-  };
-  xhttp.open("GET", "index.html", true);
-  xhttp.send();
+function retrieveRoutedPage(page, section){
+var client = new XMLHttpRequest();
+var file = pages[page].url;
+client.open('GET', file);
+client.onreadystatechange = function() {
+  if (client.readyState == 4 && client.status == 200) {
+  var data = client.responseText;
+  document.getElementById(section).innerHTML=interpret(data);
+  test()
+ }
+}
+client.send();
 }
 
   for (i in pages) {
     if (pages[i].route==page) {
-      loadPage(pages[i].url, "content");
+      retrieveRoutedPage(pages[i].url, "content");
       eval(pages[i].callback)
     }
   }
