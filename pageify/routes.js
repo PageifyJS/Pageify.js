3b0eddcba3df215fc96e1365ced14a75f2d2ef6a
Hampton <herohamp@users.noreply.github.com>
2016-09-09 17:23:55 -0400

    Update routes.js

--- pageify/routes.js
+++ pageify/routes.js
@@ -1,13 +1,4 @@
-var querys = window.location.search.slice(1).split('&').reduce(function _reduce (/*Object*/ a, /*String*/ b) {b = b.split('=');a[b[0]] = decodeURIComponent(b[1]);return a;}, {});
-console.log(querys);
 
-var page;
-
-if (querys[Object.keys(querys)[0]]!= "undefined") {
-  page="/";
-} else {
-  page=Object.keys(querys)[0];
-}
 
 var routes = {};
 
